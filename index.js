a63daf458dc280ff14884181242fd07824b358af
Bipasha Roy <91665006+Bipasha-14@users.noreply.github.com>
2022-10-21 14:02:35 +0530

    Added dicegame files

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomImageSource= "dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImageSource2= "dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h2").innerHTML = "🚩 Player 1 Wins!";
+}
+else if(randomNumber2==randomNumber1){
+    document.querySelector("h2").innerHTML = "Draw!";
+}
+else{
+    document.querySelector("h2").innerHTML = "🚩 Player 2 Wins!";
+}
+document.querySelector("button").classList.add("btn");
